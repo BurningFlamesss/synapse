d58fd1379181a73263e8dcb712315483db67e717
AD <burningggflamesss@gmail.com>
2026-04-09 22:32:52 +0545

    feat: implement context management for agent and enhance token counting functionality

--- agent/agent.py
+++ agent/agent.py
@@ -4,14 +4,17 @@ from typing import AsyncGenerator
 from agent.events import AgentEvent, AgentEventType
 from client.llm_client import LLMClient
 from client.response import StreamEventType
+from context.manager import ContextManager
 
 
 class Agent:
     def __init__(self):
         self.client = LLMClient()
+        self.context_manager = ContextManager()
         
     async def run(self, message: str):
         yield AgentEvent.agent_start(message)
+        self.context_manager.add_user_message(message)
         
         final_response: str | None = None
         
@@ -23,15 +26,10 @@ class Agent:
             
         yield AgentEvent.agent_end(final_response)
     
-    async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
-        messages = [{
-            "role": "user",
-            "content": "Hey! What are you doing?"
-        }]
-        
+    async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:        
         response_text = ""
         
-        async for event in self.client.chat_completion(messages, True):
+        async for event in self.client.chat_completion(self.context_manager.get_messages(), True):
             if event.type == StreamEventType.TEXT_DELTA:
                 if event.text_delta:
                     content = event.text_delta.content or ""
@@ -40,7 +38,8 @@ class Agent:
                 
             elif event.type == StreamEventType.ERROR:
                 yield AgentEvent.agent_error(event.error or "Unknown error occcured")
-                
+            
+        self.context_manager.add_assistant_message(response_text or None)
         if response_text:
             yield AgentEvent.text_complete(response_text)
             
